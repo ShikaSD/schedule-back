d290295778ce7dbd857f968a346a2aebbf4a27a1
ashikov <andrei.shikov@zalando.fi>
2016-09-17 01:07:16 +0300

    Use password file instead of placeholder

--- scripts/script/renew-cert.sh
+++ scripts/script/renew-cert.sh
@@ -2,11 +2,13 @@
 
 cd /opt/play/cert/
 
+password=$(cat ./password)
+
 sudo letsencrypt renew --agree-tos
 
 sudo rm -rf keystore.jks
-sudo openssl pkcs12 -export -out keystore.pkcs12 -in /etc/letsencrypt/live/schedule.northeurope.cloudapp.azure.com/fullchain.pem -inkey /etc/letsencrypt/live/schedule.northeurope.cloudapp.azure.com/privkey.pem -password pass:letsencrypt
-sudo keytool -importkeystore -srckeystore keystore.pkcs12 -srcstoretype PKCS12 -destkeystore keystore.jks -srcstorepass letsencrypt -deststorepass letsencrypt
+sudo openssl pkcs12 -export -out keystore.pkcs12 -in /etc/letsencrypt/live/schedule.northeurope.cloudapp.azure.com/fullchain.pem -inkey /etc/letsencrypt/live/schedule.northeurope.cloudapp.azure.com/privkey.pem -password pass:${password}
+sudo keytool -importkeystore -srckeystore keystore.pkcs12 -srcstoretype PKCS12 -destkeystore keystore.jks -srcstorepass ${password} -deststorepass ${password}
 sudo rm -rf keystore.pkcs12
 
 # sudo /opt/play/script/stop.sh
--- scripts/script/start-server.sh
+++ scripts/script/start-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/opt/play/current/bin/root -Dconfig.file=/opt/play/current/conf/prod.conf -Dhttps.port=443 -Dhttp.port=disabled -Dplay.server.https.keyStore.path=/opt/play/cert/keystore.jks -Dplay.server.https.keyStore.password=letsencrypt
+password=$(cat /opt/play/cert/password)
+
+/opt/play/current/bin/root -Dconfig.file=/opt/play/current/conf/prod.conf -Dhttps.port=443 -Dhttp.port=disabled -Dplay.server.https.keyStore.path=/opt/play/cert/keystore.jks -Dplay.server.https.keyStore.password=${password}
