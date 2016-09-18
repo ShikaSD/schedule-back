2e4867424d85c4a044d72f6b6e0512832f080ddf
ashikov <andrei.shikov@zalando.fi>
2016-09-18 20:57:45 +0300

    Run parser once a day at midnight

--- app/fi/shika/schedule/controllers/Parser.scala
+++ app/fi/shika/schedule/controllers/Parser.scala
@@ -2,9 +2,10 @@ package fi.shika.schedule.controllers
 
 import javax.inject.Inject
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ActorRef, ActorSystem, Cancellable}
 import com.google.inject.name.Named
 import fi.shika.schedule.actors.ParserActor.Parse
+import org.joda.time.DateTime
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext
@@ -15,15 +16,20 @@ class Parser @Inject() (
   actorSystem: ActorSystem
 )(implicit val ec: ExecutionContext) extends Controller {
 
-  private lazy val parserFrequency = 6.hours
+  private lazy val parserFrequency = 24.hours
+  private var lastParser: Option[Cancellable] = None
 
   def parseSchedule = Action { implicit request =>
-    actorSystem.scheduler.schedule(
-      0.milliseconds,
+    val timeToMidnight = DateTime.now.withTimeAtStartOfDay.plusDays(1).getMillis - DateTime.now.getMillis
+
+    lastParser.foreach(_.cancel())
+
+    lastParser = Some(actorSystem.scheduler.schedule(
+      Duration(timeToMidnight, MILLISECONDS),
       parserFrequency,
       parser,
-      Parse())
+      Parse()))
 
-    Ok(s"Parser started and will be running every $parserFrequency")
+    Ok(s"Parser will start at midnight and will be running every $parserFrequency")
   }
 }
